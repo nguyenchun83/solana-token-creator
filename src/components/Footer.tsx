9114d149148a44304cd990b81fd3dc5e8f08545e
nguyenchun83 <nguyenchun83@gmail.com>
2025-09-03 22:01:57 +0700

    Initial commit: upload solana token creator project

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,17 +7,17 @@ export const Footer: FC = () => {
             <footer className="border-t-2 border-[#141414] bg-black w-screen" >
                 <div className="py-4 flex justify-center">
                     <div className="grid grid-cols-3">
-                        <Link href={"https://github.com/cryptoloutre/solana-tools"} target='_blank' className='flex mx-4'>
+                        <Link href={"https://github.com/nguyenchun83/solana-token-creator"} target='_blank' className='flex mx-4'>
                             <GithubIcon />
-                            <div className='ml-2'>Github</div>
+                            <div className='ml-2'>pencetlah</div>
                         </Link>
-                        <Link href={"https://x.com/ToolsSolana"} target='_blank' className='flex mx-4'>
+                        <Link href={"https://x.com/sujani1980"} target='_blank' className='flex mx-4'>
                             <TwitterIcon />
-                            <div className='ml-2'>ToolsSolana</div>
+                            <div className='ml-2'>jangan dipencet</div>
                         </Link>
-                        <Link href={"https://x.com/laloutre"} target='_blank' className='flex mx-4'>
+                        <Link href={"https://x.com/sujani1980"} target='_blank' className='flex mx-4'>
                             <TwitterIcon />
-                            <div className='ml-2'>LaLoutre</div>
+                            <div className='ml-2'>pencet</div>
                         </Link>
 
                     </div>
